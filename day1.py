363da558417777d7e66c583aa8b6fca7d4ff19a8
Dragoi Andrei Marius <97879719+DragoiAM@users.noreply.github.com>
2024-03-15 17:50:01 +0200

    Day1

---
+++ day1.py
@@ -0,0 +1,52 @@
+def day1_tasks():
+    # Task 1
+    print("Hello XYZ")
+
+    # Task 2
+    print()
+
+    # Task 3
+    print("print multiple things, like:")
+    print("i like cats!")
+    print("But also like dogs as well")
+    print("")
+    print("printing " + "multiple " + " things" + " with" + " only" + " one" + " print" + " command.")
+    print("")
+
+    # Task 4
+    poem = """Nature's first green is gold,
+    Her hardest hue to hold.
+    Her early leaf's a flower;
+    But only so an hour.
+    Then leaf subsides to leaf.
+    So Eden sank to grief,
+    So dawn goes down to day.
+    Nothing gold can stay."""
+    print(poem)
+
+    # Task 5
+    input()
+    input("Here you can ask from the user in one line and (s)he can answer:")
+    input("Here you can ask from the user and in the new line (s)he can answer:\n")
+    input("What is your name?:")
+    input("How old are you?:")
+    input("Which of the following pets is your favorite? Choose one: dog, cat, hamster:\n")
+    input("Which country do you live in?:\n")
+    input("")
+
+    # Task 6
+    """
+    If you want to add multiple lines of comments, 
+    you should use the triple quotes like on this comment
+    """
+    # This is a single line comment
+
+    # Task 7 - Python console
+    pass
+
+    # Task 8-11
+    print(input("Hello World"))
+    print(input("Give me a poem:\n").replace('a', 'e'))
+    print(input("Give me a poem:\n").upper())
+    print(input("Give me a poem:\n").title()) #make the first letter in each word upper case
+    print(input("Give me a poem:\n").capitalize()) #upper case the first letter in this sentence
---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+from day1 import day1_tasks
+
+def main():
+    print("This is the main file.")
+    day1_tasks()
+
+if __name__ == "__main__":
+    main()
